f128d9684fffec52b23a4758aeb329b5e4ea7cb4
Sean Brabdury <sean.bradbury@remarkable.net>
2025-09-14 08:49:39 +0100

    changed to use cheerio & node fetch

---
+++ netlify/functions/scrape.js
@@ -0,0 +1,45 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+exports.handler = async function () {
+  try {
+    const res = await fetch('https://www.ufc.com/events');
+    const html = await res.text();
+    const $ = cheerio.load(html);
+
+    const eventDetails = [];
+    $('.c-card-event--result').each((_, el) => {
+      const headline = $(el)
+        .find('.c-card-event--result__headline')
+        .text()
+        .trim();
+      const cardUrl = $(el)
+        .find('.c-card-event--result__headline a')
+        .attr('href');
+      const prelimsTimestamp = $(el)
+        .find('.c-card-event--result__date')
+        .attr('data-prelims-card-timestamp');
+      const mainTimestamp = $(el)
+        .find('.c-card-event--result__date')
+        .attr('data-main-card-timestamp');
+      eventDetails.push({
+        headline,
+        cardUrl: cardUrl ? `https://www.ufc.com${cardUrl}` : '',
+        prelimsTimestamp: Number(prelimsTimestamp),
+        mainTimestamp: Number(mainTimestamp),
+      });
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ eventDetails }),
+      headers: { 'Content-Type': 'application/json' },
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+      headers: { 'Content-Type': 'application/json' },
+    };
+  }
+};
