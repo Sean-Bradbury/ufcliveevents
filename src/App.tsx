aaa01ae6f91904961b8c44274808c7f46d2cfa22
Sean Brabdury <sean.bradbury@remarkable.net>
2025-09-14 13:42:10 +0100

    added share & cal button, switch fetch based on environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import UFClogo from './assets/UFC_Logo.svg'; // adjust path
+import UFClogo from './assets/UFC_Logo.svg';
+import whatsAPP from './assets/whatsapp-share-button-icon.svg';
+import gCalIcon from './assets/g-cal-icon.svg';
 
 function App() {
   interface EventDetails {
@@ -17,7 +19,11 @@ function App() {
   const handleScrape = async () => {
     setLoading(true); // <-- Start loading
     try {
-      const res = await fetch('/.netlify/functions/scrape');
+      // fetch url to be different if local vs production
+      const url = window.location.hostname.includes('localhost')
+        ? '/api/scrape'
+        : '/.netlify/functions/scrape';
+      const res = await fetch(url);
       const data = await res.json();
 
       console.log(data);
@@ -45,9 +51,16 @@ function App() {
               className={`event-card ${
                 new Date(event.prelimsTimestamp * 1000).getHours() >=
                   8 &&
-                new Date(event.prelimsTimestamp * 1000).getHours() <
-                  16
+                new Date(event.prelimsTimestamp * 1000).getHours() <=
+                  17
                   ? 'should-watch'
+                  : new Date(
+                      event.prelimsTimestamp * 1000
+                    ).getHours() >= 17 &&
+                    new Date(
+                      event.prelimsTimestamp * 1000
+                    ).getHours() <= 19
+                  ? 'probably-watch'
                   : 'might-sleep'
               }`}
             >
@@ -72,6 +85,46 @@ function App() {
                 >
                   View Event Details
                 </a>
+                <div className="interactive-icons">
+                  <a
+                    className="whatsapp-share"
+                    href={`https://wa.me/?text=${encodeURIComponent(
+                      `Check out this UFC event: ${event.headline}\n${event.cardUrl}`
+                    )}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={whatsAPP} alt="WhatsApp" height="30" />
+                  </a>
+                  <a
+                    className="calendar-share"
+                    href={`https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
+                      event.headline
+                    )}&dates=${new Date(
+                      Number(event.prelimsTimestamp) * 1000
+                    )
+                      .toISOString()
+                      .replace(/[-:]|\.\d{3}/g, '')}/${new Date(
+                      (Number(event.prelimsTimestamp) + 18000) * 1000 //  5 hours duration
+                    )
+                      .toISOString()
+                      .replace(
+                        /[-:]|\.\d{3}/g,
+                        ''
+                      )}&details=${encodeURIComponent(
+                      event.cardUrl
+                    )}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ marginLeft: '10px', fontSize: '1rem' }}
+                  >
+                    <img
+                      src={gCalIcon}
+                      alt="Google Calendar"
+                      height="30"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           ))}
